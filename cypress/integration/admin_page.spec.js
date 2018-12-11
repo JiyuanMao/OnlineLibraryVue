0d350223818660ee8e844b5f56b7b8c9e2c892e2
Jiyuan Mao <1078120230@qq.com>
2018-12-11 09:27:40 +0000

    CI - Auto deploy to Surge

--- cypress/integration/admin_page.spec.js
+++ cypress/integration/admin_page.spec.js
@@ -4,60 +4,7 @@ describe('The Admin Page Test', function () {
   })
 
   it('Redirect to home', function () {
-    cy.server()
-    cy.route({
-      method: 'POST',
-      url: '/#/users/login',
-      response: {
-        data: [{
-          username: 'test',
-          password: '123',
-          usertype: 'admin'
-        }]
-      }
-    })
-    cy.route({
-      method: 'GET',
-      url: '/#/books',
-      response: [
-        {
-          'likes': 4,
-          '_id': '5c0ba01682b4d61bb00e1001',
-          'name': 'Digital Portrait Photography For Dummies',
-          'author': 'Doug Sahlin',
-          'publisher': 'John Wiley & Sons',
-          'category': 'Photography',
-          '__v': 0
-        },
-        {
-          'likes': 0,
-          '_id': '5c0ba04b82b4d61bb00e1002',
-          'name': 'Foundations for Analytics with Python',
-          'author': 'Brownley, Clinton W.',
-          'publisher': 'Southeast University Press',
-          'category': 'Computing Science',
-          '__v': 0
-        },
-        {
-          'likes': 0,
-          '_id': '5c0ba05282b4d61bb00e1003',
-          'name': 'Multi-objective Decision Analysis',
-          'author': 'Brownley, Clinton W.',
-          'publisher': 'Business Expert Pr',
-          'category': 'software engineering',
-          '__v': 0
-        },
-        {
-          'likes': 0,
-          '_id': '5c0e41cc09536e51bcd8c91d',
-          'name': 'Building Web Sites All-in-One Desk Reference For Dummies',
-          'author': 'Doug Sahlin',
-          'publisher': 'John Wiley & Sons',
-          'category': 'Computing Science',
-          '__v': 0
-        }
-      ]
-    })
+    cy.url()
   })
 
   it('Do Admin Login', function () {
