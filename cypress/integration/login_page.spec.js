bc8f9f49186ae868949786db26b87a5f102498fb
Jiyuan Mao <1078120230@qq.com>
2018-12-11 11:10:21 +0000

    test

--- cypress/integration/login_page.spec.js
+++ cypress/integration/login_page.spec.js
@@ -1,40 +1,44 @@
 describe('The Login and Register Page Test', function () {
-  it('successfully loads', function () {
-    cy.visit('/#/login') // change URL to match your dev URL
-  })
+    it('successfully loads', function () {
+        cy.visit('/#/login') // change URL to match your dev URL
+    });
 
-  it('login page card', () => {
-    cy.get('.card-header')
-  })
+    it('login page card', () => {
+        cy.get('.card-header')
+    });
 
-  it('check login button', () => {
-    cy.get('.btn-success')
-      .contains('Submit')
-  })
+    it('check login button', () => {
+        cy.get('.btn-success')
+            .contains('Submit')
+    });
 
-  it('click to register', () => {
-    cy.get('[aria-selected="false"] ')
-      .click()
-  })
+    it('click to register', () => {
+        cy.get('[aria-selected="false"] ')
+            .click()
+    });
 
-  it('check login and register visible', () => {
-    cy.get('.btn-primary')
-      .should('be.visible')
-      .contains('Register')
-    cy.get(('.btn-success'))
-      .should('not.be.visible')
-      .contains('Submit')
-  })
+    it('check login and register visible', () => {
+        cy.get('.btn-primary')
+            .should('be.visible')
+            .contains("Register");
+        cy.get(('.btn-success'))
+            .should('not.be.visible')
+            .contains("Submit")
+    });
 
-  it('back to login', () => {
-    cy.get('[aria-selected="false"] ')
-      .click()
-  })
+    it('back to login', () => {
+        cy.get('[aria-selected="false"] ')
+            .click()
+    });
 
-  it('input the username and password', () => {
-    cy.get('input[type=text]:visible').type('justin')
-    cy.get('input[type=password]:visible').type('123456')
-    cy.get('button[type=submit]:visible').click()
-  })
+    it('input the username and password', () => {
+        cy.get('input[type=text]:visible').type('justin');
+        cy.get('input[type=password]:visible').type('123456');
+        cy.get('button[type=submit]:visible').click()
+    });
 
-})
+    it('check is login success', () => {
+        cy.location('href').should('not.include', 'login')
+        cy.location('href').should('include', 'AllBooks')
+    });
+});
